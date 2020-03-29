56519c0878d59f2479679963adc69c595cd0660b
conorbailey90 <conbailey90@gmail.com>
2020-03-29 13:39:15 +0100

    Initial commit

--- passportConfig.js
+++ passportConfig.js
@@ -46,7 +46,15 @@ function initialize(passport) {
       authenticateUser
     )
   );
+  // Stores user details inside session. serializeUser determines which data of the user
+  // object should be stored in the session. The result of the serializeUser method is attached
+  // to the session as req.session.passport.user = {}. Here for instance, it would be (as we provide
+  //   the user id as the key) req.session.passport.user = {id: 'xyz'}
   passport.serializeUser((user, done) => done(null, user.id));
+
+  // In deserializeUser that key is matched with the in memory array / database or any data resource.
+  // The fetched object is attached to the request object as req.user
+
   passport.deserializeUser((id, done) => {
     pool.query(`SELECT * FROM users WHERE id = $1`, [id], (err, results) => {
       if (err) {
